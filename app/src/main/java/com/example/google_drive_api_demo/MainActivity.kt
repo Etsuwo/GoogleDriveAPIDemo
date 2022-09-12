eb1ba4c119ed3cb5e9cc3e64c5cf5b0e85665966
Etsushi <etsushi.otani@emoshu.co.jp>
2022-09-12 21:01:58 +0900

    sign in with google実装

--- app/src/main/java/com/example/google_drive_api_demo/MainActivity.kt
+++ app/src/main/java/com/example/google_drive_api_demo/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.google_drive_api_demo
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private val handleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK && it.data != null) {
+            Toast.makeText(this, "ログイン成功！", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "ログイン失敗...", Toast.LENGTH_LONG).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
         setContentView(R.layout.activity_main)
+        setupUI()
+    }
+
+    private fun setupUI() {
+        findViewById<SignInButton>(R.id.button).setOnClickListener { signInWithGoogle() }
+    }
+
+    private fun signInWithGoogle() {
+        handleLoginResult.launch(googleSignInClient.signInIntent)
     }
 }
