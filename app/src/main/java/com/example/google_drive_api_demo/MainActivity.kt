429194faf10af34f4606d79d05221eb1668f86d8
Etsushi <etsushi.otani@emoshu.co.jp>
2022-09-21 00:29:35 +0900

    MainActivityからはAPIProviderを呼び出す

--- app/src/main/java/com/example/google_drive_api_demo/MainActivity.kt
+++ app/src/main/java/com/example/google_drive_api_demo/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.google_drive_api_demo.api.DriveAPIProvider
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -29,7 +30,6 @@ import java.util.Collections
 
 class MainActivity : AppCompatActivity() {
     private lateinit var googleSignInClient: GoogleSignInClient
-    private var drive: Drive? = null
 
     // Googleアカウントへのログイン（認証）
     private val handleLoginResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
     private val handleRecoverableResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == Activity.RESULT_OK) {
             Toast.makeText(this, "認可完了", Toast.LENGTH_LONG).show()
-            queryFiles()
+            fetchFiles()
         }
     }
 
@@ -69,32 +69,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun connectDrive(intent: Intent) {
-        GoogleSignIn.getSignedInAccountFromIntent(intent)
-            .addOnSuccessListener {
-                val credential = GoogleAccountCredential.usingOAuth2(
-                    this, Collections.singleton(DriveScopes.DRIVE_READONLY)
-                )
-                credential.selectedAccount = it.account
-                drive = Drive.Builder(
-                    NetHttpTransport(),
-                    GsonFactory(),
-                    credential
-                ).setApplicationName(getString(R.string.app_name)).build()
-                queryFiles()
-            }
-            .addOnFailureListener {
-                Toast.makeText(this, "アクセス失敗だよ〜", Toast.LENGTH_LONG).show()
-            }
-    }
-
-    private fun queryFiles() {
         CoroutineScope(Dispatchers.Main).launch {
             try {
-                val fileList = execute()
+                DriveAPIProvider.connectDrive(intent)
+                fetchFiles()
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity, e.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun fetchFiles() {
+        CoroutineScope(Dispatchers.Main).launch {
+            try {
+                val fileList = DriveAPIProvider.queryFiles()
                 val builder = StringBuilder()
-                fileList.files.forEach {
-                    builder.append(it.name).append("\n")
-                }
+                fileList.files.forEach { builder.append(it.name).append("\n") }
                 Toast.makeText(this@MainActivity, builder.toString(), Toast.LENGTH_LONG).show()
             } catch (e: Exception) {
                 when (e) {
@@ -105,13 +95,5 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-    }
-
-    // rootはマイドライブのルートフォルダIDのエイリアス
-    // https://developers.google.com/drive/api/guides/search-files
-    private suspend fun execute(): FileList = withContext(Dispatchers.IO) {
-        drive?.let {
-            return@withContext it.files().list().setSpaces("drive").setQ("'root' in parents").execute()
-        } ?: run { throw IllegalAccessException() }
     }
 }
